76f309f1b423a9764f047938271ef2ae9b864871
yair <y08git>
2023-05-11 00:09:03 -0600

    Agregando esqueleto

---
+++ Assets/camera/script/cameraMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    Transform target;
+    Transform cam;
+    public float limitUpperY;
+    public float limitLowerY;
+    public Vector3 positionRelativeToTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        cam = this.transform;
+        cam.position = target.position + positionRelativeToTarget;
+        cam.LookAt(target.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cam.position.y > limitLowerY && cam.position.y < limitUpperY) //revisa que la cámara este en rango de y, para que no se vaya muy arriba ni muy abajo
+        {
+            cam.position = target.position + positionRelativeToTarget;
+        }
+    }
+}
--- Assets/player/controller/Movement.cs
+++ Assets/player/controller/Movement.cs
@@ -5,13 +5,88 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
+    Vector3 facing;
+    Rigidbody _rb;
+    Transform _tr;
+    bool canJump;
+    public float movementSpeed;
+    public float jumpHeight;
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+    public KeyCode shoot;
     void Start()
     {
-        
+        canJump = true;
+        _rb = GetComponent<Rigidbody>(); 
+        _tr = GetComponent<Transform>(); 
+        facing = new Vector3(1,0,0);
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (Input.GetKeyDown(jump) && canJump)
+        {
+            canJump = false;
+            Debug.Log("Jumped?");
+            _rb.AddForce(jumpHeight * Vector3.up, ForceMode.VelocityChange);
+        }
+        /*
+        if (Input.GetKeyDown(up))
+        {
+            _tr.LookAt(_tr.position + Vector3.forward);
+        }
+        if (Input.GetKeyDown(down))
+        {
+            _tr.LookAt(_tr.position + Vector3.back);
+        }
+        if (Input.GetKeyDown(left))
+        {
+            _tr.LookAt(_tr.position + Vector3.left);
+        }
+        if (Input.GetKeyDown(right))
+        {
+            _tr.LookAt(_tr.position + Vector3.right);
+        }*/
+
+
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(up))
+        {
+            _rb.AddForce(Vector3.forward * movementSpeed, ForceMode.VelocityChange);
+            _tr.LookAt(_tr.position + Vector3.forward);
+        } 
+        else if (Input.GetKey(down))
+        {
+            _rb.AddForce(Vector3.back * movementSpeed, ForceMode.VelocityChange);
+            _tr.LookAt(_tr.position + Vector3.back);
+        }
+        if (Input.GetKey(left))
+        {
+            _rb.AddForce(Vector3.left * movementSpeed, ForceMode.VelocityChange);
+            _tr.LookAt(_tr.position + Vector3.left);
+        }
+        else if (Input.GetKey(right))
+        {
+            _rb.AddForce(Vector3.right * movementSpeed, ForceMode.VelocityChange);
+            _tr.LookAt(_tr.position + Vector3.right);
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "ground")
+        {
+            canJump = true;
+        }
+    }
+    void pistolMovement()
     {
         
     }
